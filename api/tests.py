5ca530806ca2fff6dd51de409fed95f14e04ee14
solomonbaez <solomon.james.baez@gmail.com>
2023-08-14 11:49:53 -0700

    Added tests for NotesView

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from .models import Note
+from .serializers import NoteSerializer
 
-# Create your tests here.
+
+class NoteTests(APITestCase):
+    def test_health_check(self):
+        url = reverse("health")
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_notes(self):
+        url = reverse("notes")
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, {"message": "No notes."})
