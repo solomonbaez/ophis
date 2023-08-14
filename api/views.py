77b28e1853b849f08a9b78afb062f818e2508e66
solomonbaez <solomon.james.baez@gmail.com>
2023-08-14 11:11:23 -0700

    Added PUT and DELETE methods to NoteView

--- api/views.py
+++ api/views.py
@@ -48,3 +48,22 @@ class NoteView(APIView):
         note_data = note_data.data
 
         return Response(note_data)
+
+    def put(self, request, pk) -> Response:
+        note = Note.objects.get(id=pk)
+        note_data = NoteSerializer(instance=note, data=request.data)
+
+        if note_data.is_valid():
+            note_data.save()
+            note_data = note_data.data
+
+            return Response(note_data)
+
+        else:
+            return Response({"message": "Invalid response."})
+
+    def delete(self, request, pk) -> Response:
+        note = Note.objects.get(id=pk)
+        note.delete()
+
+        return Response({"message": "Note deleted."})
