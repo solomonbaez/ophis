6dfd8ec814262f9c74bfbdcaa748dee9e30f564b
solomonbaez <solomon.james.baez@gmail.com>
2023-08-26 21:55:51 -0700

    Cleaned up styling on individual note endpoints

--- next/src/app/note/[id]/page.tsx
+++ next/src/app/note/[id]/page.tsx
@@ -51,15 +51,23 @@ export default function Note({ params }: { params: { id: string } }) {
     router.refresh();
   };
 
+  const textHandler = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    event.preventDefault();
+    const element = event.target;
+    element.style.height = "auto";
+    element.style.height = `${element.scrollHeight}px`;
+    setNote({ ...note, content: element.value });
+  };
+
   return (
-    <div className="px-2 py-2 mb-2 h-screen rounded-md border border-gray-300 bg-gray-600">
+    <div className="px-2 py-2 mb-2 h-1/2 rounded-md border border-gray-300 bg-gray-600">
       {note.title}
       <hr />
       <textarea
-        className="bg-gray-600 max-w-sm"
+        className="w-full h-auto bg-gray-600 resize-none"
         defaultValue={note?.content}
         onChange={(event) => {
-          setNote({ ...note, content: event.target.value });
+          textHandler(event);
         }}
       >
         {note?.content}
