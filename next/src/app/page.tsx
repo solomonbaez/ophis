b6d9f555a01d2ddbf02982ac04ae25cacdde4b36
solomonbaez <solomon.james.baez@gmail.com>
2023-08-28 10:02:29 -0700

    Fixed automatic element updates on note deletion

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -9,40 +9,14 @@ interface NoteItemProps {
   note: Note;
 }
 
-const NoteItem: React.FC<NoteItemProps> = ({ note }) => {
-  const truncatedContent =
-    note.content.length < 40 ? note.content : note.content.slice(0, 40);
-
-  const handleDelete = (id: string) => {
-    DeleteNote(id);
-    // getNotes();
-  };
-
-  return (
-    <li className="px-2 py-2 mb-2 rounded-md border border-gray-300 bg-gray-600">
-      <Link href={`note/${note.id}`} passHref>
-        <button className="hover:text-blue-400 hover:underline">
-          {note.title}
-        </button>
-      </Link>
-      <br />
-      {truncatedContent}
-      <br />
-      {note.updated}
-      <br />
-      <button
-        className="text-red-500 hover:text-red-600 hover:underline"
-        onClick={() => handleDelete(note.id)}
-      >
-        delete
-      </button>
-      <hr />
-    </li>
-  );
-};
-
 const Home: React.FC = () => {
   let [notes, setNotes] = useState<Note[]>([]);
+
+  const truncatedContent = (content: string) => {
+    let truncated: string =
+      content.length < 40 ? content : content.slice(0, 40);
+    return truncated;
+  };
 
   const getNotes = () => {
     fetch("http://127.0.0.1:8000/api/notes/")
@@ -56,6 +30,13 @@ const Home: React.FC = () => {
     getNotes();
   }, []);
 
+  const handleDelete = (id: string) => {
+    DeleteNote(id);
+
+    const updatedNotes = notes.filter((note) => note.id !== id);
+    setNotes(updatedNotes);
+  };
+
   return (
     <main>
       <Head>
@@ -65,7 +46,28 @@ const Home: React.FC = () => {
         <h1 className="text-center">NOTES</h1>
         <ul>
           {notes.map((note, index) => (
-            <NoteItem key={index} note={note} />
+            <li
+              key={index}
+              className="px-2 py-2 mb-2 rounded-md border border-gray-300 bg-gray-600"
+            >
+              <Link href={`note/${note.id}`} passHref>
+                <button className="hover:text-blue-400 hover:underline">
+                  {note.title}
+                </button>
+              </Link>
+              <br />
+              {truncatedContent(note.content)}
+              <br />
+              {note.updated}
+              <br />
+              <button
+                className="text-red-500 hover:text-red-600 hover:underline"
+                onClick={() => handleDelete(note.id)}
+              >
+                delete
+              </button>
+              <hr />
+            </li>
           ))}
         </ul>
         <Link href="/note/create" passHref>
